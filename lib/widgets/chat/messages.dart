227215e809fd8c4690679f0911a431e552a39a34
neptune0170 <yg172014@gmail.com>
2021-06-29 23:26:40 +0530

    v18 completed

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -5,7 +5,10 @@ class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
       builder: (ctx, chatSnapshot) {
         if (chatSnapshot.connectionState == ConnectionState.waiting) {
           return Center(
@@ -15,6 +18,7 @@ class Messages extends StatelessWidget {
         //try to change documents to docs
         final chatDocs = chatSnapshot.data.docs;
         return ListView.builder(
+            reverse: true,
             itemCount: chatDocs.length,
             itemBuilder: (ctx, index) => Text(
                   chatDocs[index]['text'],
--- lib/widgets/chat/new_message.dart
+++ lib/widgets/chat/new_message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class NewMessage extends StatefulWidget {
   @override
@@ -6,13 +7,40 @@ class NewMessage extends StatefulWidget {
 }
 
 class _NewMessageState extends State<NewMessage> {
+  final _controller = new TextEditingController();
+  var _enteredMessage = '';
+  void _sendMessage() {
+    FocusScope.of(context).unfocus();
+    FirebaseFirestore.instance
+        .collection('chat')
+        .add({'text': _enteredMessage, 'createdAt': Timestamp.now()});
+    _controller.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 8),
       padding: EdgeInsets.all(8),
       child: Row(
-        children: [Expanded(child: TextField())],
+        children: [
+          Expanded(
+            child: TextField(
+              controller: _controller,
+              decoration: InputDecoration(labelText: 'Type a message'),
+              onChanged: (value) {
+                setState(() {
+                  _enteredMessage = value;
+                });
+              },
+            ),
+          ),
+          IconButton(
+            color: Theme.of(context).primaryColor,
+            icon: Icon(Icons.send),
+            onPressed: _enteredMessage.trim().isEmpty ? null : _sendMessage,
+          )
+        ],
       ),
     );
   }
