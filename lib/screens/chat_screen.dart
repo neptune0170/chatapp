1cc815dafde95a2fe250d966a0b2e7aff9dd9700
neptune0170 <yg172014@gmail.com>
2021-06-29 20:27:31 +0530

    v-15 competed

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+//
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
