04abb89d9cb819a3e379e345a0201b9335b8c993
neptune0170 <yg172014@gmail.com>
2021-06-30 22:20:31 +0530

    v 27 completed

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -33,6 +33,7 @@ class Messages extends StatelessWidget {
                 itemBuilder: (ctx, index) => MessageBubble(
                   message: chatDocs[index]['text'],
                   userName: chatDocs[index]['username'],
+                  userImage: chatDocs[index]['userImage'],
                   isMe: chatDocs[index]['userId'] == futureSnapshot.data.uid,
                   // key: ValueKey(chatDocs[index].userID),
                 ),
